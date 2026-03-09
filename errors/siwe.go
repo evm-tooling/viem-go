38deb83f8e0d405c641730af11930ad7d9cf7c52
Evan McGrane <evanmcgranebb98@gmail.com>
2026-03-09 20:26:46 +0000

    feat: add remaining error entities

---
+++ errors/siwe.go
@@ -0,0 +1,21 @@
+package errors
+
+import "fmt"
+
+// SiweInvalidMessageFieldError is returned when SIWE message field is invalid.
+type SiweInvalidMessageFieldError struct {
+	Field       string
+	DocsPath    string
+	MetaMessages []string
+}
+
+func (e *SiweInvalidMessageFieldError) Error() string {
+	msg := fmt.Sprintf("Invalid Sign-In with Ethereum message field \"%s\".", e.Field)
+	for _, m := range e.MetaMessages {
+		msg += "\n" + m
+	}
+	if e.DocsPath != "" {
+		msg += fmt.Sprintf("\nDocs: https://viem.sh%s", e.DocsPath)
+	}
+	return msg
+}
